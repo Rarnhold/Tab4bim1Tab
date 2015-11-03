4aecc416a39a8357c91db20a936574270b023d4d
Arnhold <Arnhold@Arnhold-PC>
2015-11-03 19:41:13 -0200

    Cadastro Classe Produto

--- src/main/java/br/arnhold/cadastro/model/Produto.java
+++ src/main/java/br/arnhold/cadastro/model/Produto.java
@@ -1,5 +1,7 @@
 package br.arnhold.cadastro.model;
 
+import java.math.BigDecimal;
+
 //Id, código de barras, categoria (LIMPEZA, PEÇAS, ALIMENTAÇÃO, ETC), descrição, unidade (KG, UN, PCT, CX), custo (BigDecimal) e margem de lucro (BigDecimal).
 
 public class Produto {
@@ -7,9 +9,64 @@ public class Produto {
 	private int codBarras;
 	private Categoria categoria;
 	private String descricao;
-	
-	
-	
-	
-	
+	private Unidade unidadeEmbalagem;
+	private BigDecimal custo;
+	private BigDecimal margemLucro;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCodBarras() {
+		return codBarras;
+	}
+
+	public void setCodBarras(int codBarras) {
+		this.codBarras = codBarras;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Unidade getUnidadeEmbalagem() {
+		return unidadeEmbalagem;
+	}
+
+	public void setUnidadeEmbalagem(Unidade unidadeEmbalagem) {
+		this.unidadeEmbalagem = unidadeEmbalagem;
+	}
+
+	public BigDecimal getCusto() {
+		return custo;
+	}
+
+	public void setCusto(BigDecimal custo) {
+		this.custo = custo;
+	}
+
+	public BigDecimal getMargemLucro() {
+		return margemLucro;
+	}
+
+	public void setMargemLucro(BigDecimal margemLucro) {
+		this.margemLucro = margemLucro;
+	}
+
 }
